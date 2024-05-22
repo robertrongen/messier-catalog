40b68c2e545787dfcf67071d996a428ae0a5bfad
Robert Rongen <robert.rongen@ziggo.nl>
2024-05-22 11:30:14 +0200

    Fix webhook

--- backend/webhook.py
+++ backend/webhook.py
@@ -6,32 +6,41 @@ import hmac
 import hashlib
 from dotenv import load_dotenv
 
-load_dotenv()
+app = Flask(__name__)
+
+load_dotenv(dotenv_path='/home/robert/github/deepsky/backend/.env')
 WEBHOOK_SECRET = os.getenv('WEBHOOK_SECRET')
 
-app = Flask(__name__)
+def verify_signature(request):
+    signature = request.headers.get('X-Hub-Signature-256')
+    if not signature:
+        abort(400, 'Signature header missing')
+
+    sha_name, signature = signature.split('=')
+    if sha_name != 'sha256':
+        abort(400, 'Invalid signature format')
+
+    mac = hmac.new(WEBHOOK_SECRET.encode(), msg=request.data, digestmod=hashlib.sha256)
+    if not hmac.compare_digest(mac.hexdigest(), signature):
+        abort(400, 'Invalid signature')
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        # Verify the request signature
-        signature = request.headers.get('X-Hub-Signature-256')
-        if not signature:
-            abort(400, 'Signature header missing')
+        verify_signature(request)
 
-        sha_name, signature = signature.split('=')
-        if sha_name != 'sha256':
-            abort(400, 'Invalid signature format')
+        payload = request.get_json()
+        commits = payload.get('commits', [])
+        skip_build = any('[skip ci]' in commit.get('message', '').lower() for commit in commits)
 
-        mac = hmac.new(WEBHOOK_SECRET.encode(), msg=request.data, digestmod=hashlib.sha256)
-        if not hmac.compare_digest(mac.hexdigest(), signature):
-            abort(400, 'Invalid signature')
+        if skip_build:
+            return 'Build skipped due to commit message', 200
 
         # Trigger the deployment script
         subprocess.call(['bash', '~/github/deepsky/deploy.sh'])
         return 'Webhook received and deployment script triggered', 200
     else:
         abort(405)
-
+        
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
